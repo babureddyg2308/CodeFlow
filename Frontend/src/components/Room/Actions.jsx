a127f7edebacf920671cf1b2493a31198abd3622
shugaur <104693036+shugaur@users.noreply.github.com>
2024-06-15 23:56:43 +0530

    Day-4

---
+++ Frontend/src/components/Room/Actions.jsx
@@ -0,0 +1,9 @@
+
+export const ACTIONS = {
+    JOIN: "join",
+    JOINED: "joined",
+    DISCONNECTED: "disconnected",
+    CODE_CHANGE: "conde-change",
+    SYNC_CODE: "sync-code",
+    LEAVE: "leave",
+  };
